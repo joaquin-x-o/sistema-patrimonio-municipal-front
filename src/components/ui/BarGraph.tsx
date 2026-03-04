21b4c9efd61c5f147aa2373cf4efd8a22be04529
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-04 14:51:28 -0300

    feat: add bar graph component

---
+++ src/components/ui/BarGraph.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+    label: string;
+    value: number;
+    max: number;
+}
+
+export function Bar({ label, value, max }: Props) {
+    // calculo porcentaje
+    const percentage = Math.round((value / max) * 100);
+
+    // animacion barra
+
+    const [animatedWidth, setAnimatedWidth] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setAnimatedWidth(percentage);
+        }, 100);
+
+        return () => clearTimeout(timer);
+    }, [percentage]);
+
+    return (
+        <div className="flex items-center gap-4 mb-4 last:mb-0">
+            {/* label */}
+            <div className="w-24 text-sm text-foreground-muted truncate">
+                {label}
+            </div>
+
+            {/* barra */}
+            <div className="flex-1 h-6 flex items-center">
+                <div
+                    className="h-full bg-primary-hover transition-all duration-1000 ease-out"
+                    style={{ width: `${animatedWidth}%` }}
+                ></div>
+            </div>
+
+            {/* valor que representa la barra */}
+            <div className="w-10 text-right text-sm text-slate-600">
+                {value}
+            </div>
+        </div>
+    );
+}
