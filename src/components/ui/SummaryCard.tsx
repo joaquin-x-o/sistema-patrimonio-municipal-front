8d83269ce8839cddffa74a70a4a0c863fff0af0e
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-05 20:02:56 -0300

    feat(SummaryCard): add link feature to summary card component

--- src/components/ui/SummaryCard.tsx
+++ src/components/ui/SummaryCard.tsx
@@ -1,15 +1,17 @@
 import type { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
     title: string;
     value: string | number;
     icon: ReactNode;
     bgColor: string;
+    to: string
 }
 
-export function SummaryCard({ title, value, icon, bgColor }: Props) {
-    return (
+export function SummaryCard({ title, value, icon, bgColor, to }: Props) {
 
+    const cardContent = (
         <div className={`${bgColor} rounded-xl p-6 text-white flex justify-between items-center shadow-lg transition-transform hover:scale-[1.02] overflow-x-hidden cursor-pointer`}>
             <div className="flex flex-col min-w-0 flex-1">
                 <span className="text-sm font-bold uppercase tracking-wider opacity-100 truncate">
@@ -25,5 +27,12 @@ export function SummaryCard({ title, value, icon, bgColor }: Props) {
                 {icon}
             </div>
         </div>
+    )
+
+    return (
+        <Link to={to}>
+            {cardContent}
+        </Link>
     );
+
 }
