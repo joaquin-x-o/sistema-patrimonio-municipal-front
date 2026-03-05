d8be5f4801c132c82456f331b6176a2b6bb4d6ab
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-05 18:21:49 -0300

    feat: add optional total prop to use it when the card shows a table

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,6 +3,7 @@ import { FooterLink } from "./FooterLink";
 
 interface Props {
     title: string;
+    total?: number;
     children: ReactNode;
 
     footerLinkTo?: string;
@@ -10,13 +11,19 @@ interface Props {
     onFooterClick?: () => void;
 }
 
-export function Card({ title, children, footerLinkTo, footerLinkLabel = "Ver todos", onFooterClick }: Props) {
+export function Card({ title, total, children, footerLinkTo, footerLinkLabel = "Ver todos", onFooterClick }: Props) {
     return (
         <div className="bg-white rounded-xl shadow-sm border border-slate-200 flex flex-col h-full overflow-x-hidden">
             {/* titulo */}
             <div className="p-4 border-b border-slate-200">
                 <h2 className="text-foreground-muted font-bold uppercase text-m tracking-wide">
                     {title}
+                    {/* Si total existe, se muestra entre paréntesis */}
+                    {total !== undefined && (
+                        <span className="ml-2">
+                            ({total})
+                        </span>
+                    )}
                 </h2>
             </div>
 
