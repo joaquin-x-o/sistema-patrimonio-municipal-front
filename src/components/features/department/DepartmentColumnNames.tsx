a0b41a151c5844085a275a090751b5d48be0b02e
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-05 18:38:06 -0300

    feature(DepartmentColumnNames): add DepartmentColumnNames component to be used when making department tables

---
+++ src/components/features/department/DepartmentColumnNames.tsx
@@ -0,0 +1,42 @@
+import type { ColumnDef } from "../../../interfaces/columnDef";
+import type { DepartmentResponse } from "../../../interfaces/departmentResponse";
+import { Button } from "../../ui/Button";
+
+export const DepartmentColumnNames: ColumnDef<DepartmentResponse>[] = [
+    {
+        header: "Código",
+        accessorKey: "departmentCode"
+    },
+    {
+        header: "Área",
+        accessorKey: "name"
+    },
+    {
+        header: "Responsable",
+        accessorKey: "responsibleName"
+    },
+    {
+        header: "Cantidad de bienes",
+        accessorKey: "productCount"
+    },
+    {
+        header: "Porcentaje",
+        accessorKey: "percentage",
+        cell: (row) => row.percentage ? `${row.percentage} %` : "0.0 %"
+    },
+    {
+        header: "",
+        accessorKey: "departmentCode",
+        cell: (row) => (
+            <div className="flex justify-end">
+                <Button
+                    to={`/gestion-areas/${row.departmentCode}`}
+                    variant="primary"
+                    className="px-6 py-1 text-xs"
+                >
+                    Ver
+                </Button>
+            </div>
+        )
+    }
+];
