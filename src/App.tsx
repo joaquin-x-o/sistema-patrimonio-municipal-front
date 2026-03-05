238a4e5547242a2b0901733019c69273e2cf0890
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-05 18:41:54 -0300

    refactor(App): implements AppRoutes and ScrollToTop features to be used on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import AppSidebar from "./app/AppSidebar";
 import Topbar from "./components/layout/topbar/Topbar";
-import Dashboard from "./pages/Dashboard";
 import { SidebarProvider } from "./components/layout/sidebar/SidebarProvider";
+import { AppRoutes } from "./Routes/AppRoutes";
+import { ScrollToTop } from "./hooks/useScrollToTop";
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <div className="flex h-screen bg-[#F3F4F6] overflow-hidden text-foreground font-sans">
 
       <BrowserRouter>
+        <ScrollToTop />
         <SidebarProvider>
           {/* SIDEBAR */}
           <AppSidebar />
@@ -20,7 +22,7 @@ function App() {
             <Topbar />
 
             <main className="flex-1 overflow-y-auto p-8">
-              <Dashboard />
+              <AppRoutes />
             </main>
 
           </div>
