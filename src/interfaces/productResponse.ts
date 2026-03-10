18c2691488e067a585cb01b6674ce85f58a9a31f
Joaquin-x-o <122052294+joaquin-x-o@users.noreply.github.com>
2026-03-10 17:53:29 -0300

    feat(productResponse): add productLightResponse interface and function to search products based on that response

--- src/interfaces/productResponse.ts
+++ src/interfaces/productResponse.ts
@@ -63,3 +63,20 @@ export interface ProductShortResponse {
     department: { departmentCode: string, name: string };
 }
 
+export interface ProductLightResponse {
+    productCode: number;
+    name: string;
+}
+
+
+// función para buscar un producto 
+export const searchProductsLight = (query: string, mock: ProductLightResponse[]): ProductLightResponse[] => {
+    if (!query) return mock;
+
+    const lowerQuery = query.toLowerCase();
+
+    return mock.filter(product =>
+        product.name.toLowerCase().includes(lowerQuery) ||
+        product.productCode.toString().includes(lowerQuery)
+    );
+};
